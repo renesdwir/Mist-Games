e57c70b5ad50a66ed6136ff11ff60562fff60c5c
AndrizalChaidar <andrizal.cs@gmail.com>
2022-02-24 01:45:14 +0700

    Andrizal Chaidar seed - bcrypt

--- migrations/20220224090231-create-game.js
+++ migrations/20220224090231-create-game.js
@@ -20,6 +20,10 @@ module.exports = {
       description: {
         type: Sequelize.TEXT
       },
+      
+      image: {
+        type: Sequelize.STRING
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE
--- migrations/20220224090556-create-user-game.js
+++ migrations/20220224090556-create-user-game.js
@@ -8,7 +8,7 @@ module.exports = {
         primaryKey: true,
         type: Sequelize.INTEGER
       },
-      puchased: {
+      purchased: {
         type: Sequelize.BOOLEAN
       },
       createdAt: {
