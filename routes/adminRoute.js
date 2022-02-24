d5c6c5c1c6428f2c48304f92088393b83133e97e
Renes Dwi Riwanto <renesriwanto@gmail.com>
2022-02-25 06:14:24 +0700

    Fix Routes

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -3,12 +3,12 @@ const adminRouter = express.Router();
 const UserController = require('../controllers/UserController')
 const GameController = require('../controllers/GameController')
 
-adminRouter.get('/admins/shop', GameController.getGames)
-adminRouter.get('/admins/shop/add', GameController.getAddGame)
-adminRouter.post('/admins/shop/add', GameController.postAddGame)
-adminRouter.get('/admins/shop/:GamesId', GameController.getGameDetail)
-adminRouter.get('/admins/shop/:GamesId/delete', GameController.getDeleteGame)
-adminRouter.get('/admins/shop/:GamesId/edit', GameController.getEditGame)
-adminRouter.post('/admins/shop/:GamesId/edit', GameController.postEditGame)
+adminRouter.get('/shop', GameController.getGames)
+adminRouter.get('/shop/add', GameController.getAddGame)
+adminRouter.post('/shop/add', GameController.postAddGame)
+adminRouter.get('/shop/:GamesId', GameController.getGameDetail)
+adminRouter.get('/shop/:GamesId/delete', GameController.getDeleteGame)
+adminRouter.get('/shop/:GamesId/edit', GameController.getEditGame)
+adminRouter.post('/shop/:GamesId/edit', GameController.postEditGame)
 
 module.exports = adminRouter
