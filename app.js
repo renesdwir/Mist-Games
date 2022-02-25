b816e596ff82cd34d66873f219bcedb1527071b6
Andrizal Chaidar <83231369+AndrizalChaidar@users.noreply.github.com>
2022-02-25 08:58:01 +0700

    port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = proccess.env.PORT || 3000;
 const session = require('express-session')
 const router = require('./routes/index')
 
